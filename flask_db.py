c03da0d385d9ac80560a0b899d0181140491a2c7
branjoho <brandonjho29@gmail.com>
2026-04-01 15:26:44 -0700

    fix: create new Supabase client per request to prevent concurrent connection errors

--- flask_db.py
+++ flask_db.py
@@ -3,16 +3,11 @@ import os
 from datetime import datetime, timezone, timedelta
 from supabase import create_client, Client
 
-_client = None
-
 def get_client() -> Client:
-    global _client
-    if _client is None:
-        _client = create_client(
-            os.environ["SUPABASE_URL"],
-            os.environ["SUPABASE_KEY"],
-        )
-    return _client
+    return create_client(
+        os.environ["SUPABASE_URL"],
+        os.environ["SUPABASE_KEY"],
+    )
 
 
 # ---------------------------------------------------------------------------
